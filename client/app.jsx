61dfc2b3f1fb4cab086952e658d0d92bf7e4ed57
Samuel Kwak <kwak123@gmail.com>
2017-11-06 17:21:40 -0500

    login page working, removed bundle.js from track

--- client/app.jsx
+++ client/app.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
+=======
+import Login from './components/Login.jsx'
+const axios = require('axios');
+>>>>>>> Stashed changes
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
+<<<<<<< Updated upstream
+=======
+  ComponentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    // axios.get(url);
+  }
+
+  handleLogin(username, password) {
+    console.log('username: ', username);
+    console.log('password: ', password);
+  }
+
+>>>>>>> Stashed changes
   render() {
     return (
       <div>
         <h1>Let us begin</h1>
         <h3>Bespinterest is BestPinterest</h3>
-        <Login handleLogin={() => console.log('test')}/>
+        <Login handleLogin={this.handleLogin}/>
       </div>
     );
   }
 }
 
+<<<<<<< Updated upstream
 ReactDOM.render(<App />, document.getElementById('app'));
+=======
+ReactDOM.render(<App />, document.getElementById('app'));
+>>>>>>> Stashed changes
