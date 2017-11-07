cff5fdefdf4d55489e0ffa4b58f27a85ead3d2e1
sohee Lim <limsohee1002@gmail.com>
2017-11-07 11:27:27 -0500

    nav bar created

--- client/components/Wall.jsx
+++ client/components/Wall.jsx
@@ -5,28 +5,36 @@ import Post from './Post.jsx';
 /**
  * Photowall used in both the main page and on a per user basis
  * Expects an array of posts from the wrapping app component
- * 
+ *
  * TODO: Change this to functional stateless if supported?
  */
 class Wall extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-
-  render() {
-    return (
-      <div>
-        <h1>Wall Stuff goes here</h1>
-        {this.props.posts.map((post, i) => 
-          <Post key={i} handleClick={this.props.handleClick} showInfo={this.props.showInfo} photoUrl={post.photoUrl} photoInfo={post.info} />)}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>Wall Stuff goes here</h1>
+				{this.props.posts.map((post, i) => (
+					<Post
+						key={i}
+						handleClick={this.props.handleClick}
+						showInfo={this.props.showInfo}
+						photoUrl={post.photoUrl}
+						photoInfo={post.info}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 Wall.propTypes = {
-  posts: PropTypes.array,
-  showInfo: PropTypes.bool,
-  handleClick: PropTypes.func
+	posts: PropTypes.array,
+	showInfo: PropTypes.bool,
+	handleClick: PropTypes.func
 };
+
+export default Wall;
