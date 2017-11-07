98d5ce2324ab2a4a566c919dc75e288f6bee6f85
sohee Lim <limsohee1002@gmail.com>
2017-11-07 13:12:01 -0500

    route

--- client/app.jsx
+++ client/app.jsx
@@ -9,66 +9,66 @@ import Login from './components/Login.jsx';
 const axios = require('axios');
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: []
-		};
-		this.handleLogin = this.handleLogin.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-	ComponentDidMount() {
-		this.getData();
-	}
+  ComponentDidMount() {
+    this.getData();
+  }
 
-	getData() {
-		// axios.get('/posts')
-		// .then((data) => {
-		//   console.log(data)
-		// this.setState({data: data.data})
-	}
+  getData() {
+    // axios.get('/posts')
+    // .then((data) => {
+    //   console.log(data)
+    // this.setState({data: data.data})
+  }
 
-	handleLogin(username, password) {
-		console.log('username: ', username);
-		console.log('password: ', password);
-	}
-	//need to add history
-	//in router add history={browserHistory} this will prevent reach to server??
-	//default page
-	// <IndexRoute component={Login} />
-	// <h1>Let us begin</h1>
-	// <h3>Bespinterest is BestPinterest</h3>
-	// <Login handleLogin={this.handleLogin} />
-	render() {
-		return (
-			<BrowserRouter>
-				<div>
-					<nav>
-						<ul>
-							<li>
-								<Link to={'/wall'}>Home</Link>
-							</li>
-							<li>
-								<Link to={'/user'}>User</Link>
-							</li>
-						</ul>
-					</nav>
-					<Route
-						path="/"
-						render={() => (
-							<div>
-								<h1>Let us begin</h1>
-								<h3>Bespinterest is BestPinterest</h3>
-								<Login handleLogin={this.handleLogin} />
-							</div>
-						)}
-					/>
-					<Route path="wall" render={() => <Wall />} />
-					<Route path="user" render={() => <Wall />} />
-				</div>
-			</BrowserRouter>
-		);
-	}
+  handleLogin(username, password) {
+    console.log('username: ', username);
+    console.log('password: ', password);
+  }
+  //need to add history
+  //in router add history={browserHistory} this will prevent reach to server??
+  //default page
+  // <IndexRoute component={Login} />
+  // <h1>Let us begin</h1>
+  // <h3>Bespinterest is BestPinterest</h3>
+  // <Login handleLogin={this.handleLogin} />
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={'/wall'}>Home</Link>
+              </li>
+              <li>
+                <Link to={'/user'}>User</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route
+            path="/"
+            render={() => (
+              <div>
+                <h1>Let us begin</h1>
+                <h3>Bespinterest is BestPinterest</h3>
+                <Login handleLogin={this.handleLogin} />
+              </div>
+            )}
+          />
+          <Route path="wall" render={() => <Wall />} />
+          <Route path="user" render={() => <Wall />} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
