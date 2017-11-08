a88a9619a3be7f06d4f3f26d3299a3a6294e11df
Yusaku Kasahara <yusakukasahara@gmail.com>
2017-11-08 16:52:10 -0500

    rerouted routes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = require('./router.js');
-var db = require('./db/db.js')
+var db = require('./db/db.js');
 var app = express();
 
 var logger = (req, res, next) => {
-  console.log(`Received ${req.method} request at ${req.originalUrl}`);
-  next();
+	console.log(`Received ${req.method} request at ${req.originalUrl}`);
+	next();
 };
 
 app.use(logger);
 app.use(express.static(__dirname + '/dist'));
-// app.use('/', router);
+app.use('/', router);
 
 app.listen(process.env.port || 3000, () =>
-  console.log('now listening on 3000')
+	console.log('now listening on 3000')
 );
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,12 @@ var db = require('../db/db.js');
 var express = require('express');
 var router = express.Router();
 
-//when get request sent to /, return all posts
 router.get('/', function(req, res) {
 	db.Posts.findAll().then(function(data) {
 		res.send(200, data);
 	});
 });
 
-module.exports = controller;
+router.get('/post', (req, res) => res.sendStatus(200));
+
+module.exports = router;
