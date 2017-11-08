d1239f262389ae7fffebb19b8655ef8d4b31aca0
Yusaku Kasahara <yusakukasahara@gmail.com>
2017-11-08 14:45:03 -0500

    created sequalize routes

---
+++ server/controllers/boards.js
@@ -0,0 +1,29 @@
+var models = require('../db/db.js');
+var express = require('express');
+var router = express.Router();
+
+router.post('/boards', function(req, res) {
+	db.boards
+		.create({
+			name: req.body.name
+		})
+		.then(function() {
+			console.log('Board created!');
+			res.send(true);
+			res.redirect('/');
+		});
+});
+//
+// router.get('/:user_id/boards/:board_id/destroy', function(req, res) {
+// 	db.Board
+// 		.destroy({
+// 			where: {
+// 				id: req.params.board_id
+// 			}
+// 		})
+// 		.then(function() {
+// 			res.redirect('/');
+// 		});
+// });
+
+module.exports = router;
