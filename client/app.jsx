e21461a598c0211740d7719dede0b936f8ff285b
sohee Lim <limsohee1002@gmail.com>
2017-11-08 22:42:29 -0500

    add click methods

--- client/app.jsx
+++ client/app.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, browserHistory } from 'react-router';
-import { Link, BrowserRouter, IndexRoute, HashRouter } from 'react-router-dom';
-import Popup from 'react-popup';
+// import { Route, browserHistory } from 'react-router';
+// import { Link, BrowserRouter, IndexRoute, HashRouter } from 'react-router-dom';
+// import Popup from 'react-popup';
 import Wall from './components/Wall.jsx';
 import Profile from './components/Profile.jsx';
 import Login from './components/Login.jsx';
 import Logo from './assets/Logo.jsx';
 import Main from './components/Main.jsx';
 import Addphoto from './components/Addphoto.jsx';
+import Clickphoto from './components/Clickphoto.jsx';
 
 // For testing purposes only
 import Users from './testData/usersJs.js';
@@ -22,6 +23,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showPhoto: false,
+      clickedPhoto: { photoUrl: '', photoInfo: '' },
       showPopup: false,
       isLoggedIn: true,
       user: {},
@@ -31,6 +34,9 @@ class App extends React.Component {
     this.handleLogin = this.handleLogin.bind(this);
     this.handleAddphoto = this.handleAddphoto.bind(this);
     this.handleSignup = this.handleSignup.bind(this);
+    this.handlePin = this.handlePin.bind(this);
+    this.handleAddBoard = this.handleAddBoard.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   // ComponentDidMount() {
@@ -52,19 +58,39 @@ class App extends React.Component {
         }
       })
       .then(data => {
-        axios.get('/board', { params: { id: data.boards } }).then(data => {
-          this.setState({ boards: data, isLoggedIn: true });
-        });
-      });
+        axios
+          .get('/board', { params: { id: data.id } })
+          .then(data => {
+            this.setState({ boards: data, isLoggedIn: true });
+          })
+          .then(axios.get('/post', { params: { id: -1 } }))
+          .then(data => this.setState({ posts: data }))
+          .catch(err => console.log(err));
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
     console.log('login ', username);
   }
 
-  handleAddBoard(postid, boardid) {
-    axios.post('/bespin', { postid: postid, boardid: boardid });
+  handlePin(postid, boardid) {
+    axios
+      .post('/bespin', { postid: postid, boardid: boardid })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleAddBoard(boardName, userId) {
+    axios
+      .post('/board', { name: boardName, foreignKey: userId })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
   handleSignup(username, password) {
-    console.log('signup: ', username, password);
     axios
       .post('/signup', {
         username: username,
@@ -73,9 +99,10 @@ class App extends React.Component {
       .then(res => {
         console.log(res);
       })
-      .catch(error => {
-        console.log(error);
+      .catch(err => {
+        console.log(err);
       });
+    console.log('signup: ', username, password);
   }
 
   togglePopup() {
@@ -97,20 +124,24 @@ class App extends React.Component {
       .then(res => {
         console.log(res);
       })
-      .catch(error => {
-        console.log(error);
+      .catch(err => {
+        console.log(err);
       })
       .then(
         axios
           .get('/post')
-          .then(data => {
-            setState({ posts: data });
-          })
+          .then(axios.get('/post', { params: { id: -1 } }))
+          .then(data => this.setState({ posts: data }))
           .catch(error => console.log(error))
       );
   }
 
-  handleClick() {}
+  handleClick(photoUrl, photoInfo) {
+    this.setState({
+      showphoto: !this.state.showphoto,
+      clickedPhoto: { photoUrl: photoUrl, photoInfo: photoInfo }
+    });
+  }
 
   render() {
     return (
