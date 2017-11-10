4882ae523e7c7e5e0e11fddb4e6d03f51cf072eb
Yusaku Kasahara <yusakukasahara@gmail.com>
2017-11-10 12:02:13 -0500

    refactored routes

---
+++ server/controllers/index.js
@@ -0,0 +1,9 @@
+var boards = require('./boards.js');
+var posts = require('./posts.js');
+var users = require('./users.js');
+
+module.exports = {
+	boards: boards,
+	users: users,
+	posts: posts
+};
